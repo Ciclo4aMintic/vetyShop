d0acafabbf4284fb88fa5ad09fd51a68681ab766
Yuely Rios <yuelyrios@gmail.com>
2022-10-31 00:51:33 -0500

    clase 7 octubre

--- back/server.js
+++ back/server.js
@@ -1,9 +1,10 @@
-const app =require("./app");
+const app=require("./app")
+const connectDatabase = require("./config/database");
+//Setear el archivo de configuraciónconst dotenv=require("dotenv");
+dotenv.config({path: 'back/config/config.env'})
+//Configurar base de datos
+connectDatabase();
+//Llamemos al server
+const server=app.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)})
 
-//Setear el archivo de configuracion
-const dotenv =require("dotenv");
-dotenv.config({path:'back/config/config.env'})
-
-const server=app.listen(process.env.PORT,() => {
-    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
-})
